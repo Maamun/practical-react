c30c35d9203bdc53fa9d2d6045b2dc9a86662164
Maamun <mamun.mokni@gmail.com>
2018-11-05 17:18:57 +0100

    Advanced state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Counter } from './Counter';
+import ImageSlider from './ImageSlider';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter />
+        <ImageSlider />
       </div>
     );
   }
