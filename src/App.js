762cd00fb50ce176f6a2b2942a97571ae170da50
Maamun <mamun.mokni@gmail.com>
2018-11-06 15:24:47 +0100

    Conditionally Render Components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Imageslider from './ImageSlider';
 class App extends Component {
+  state = {
+    visible: true,
+    label: 'Hide'
+  }
+  handleClick = () => {
+    this.setState(({visible, label}) => ({
+      visible: !visible
+    }))
+  }
   render() {
+    const btnText = this.state.visible ? 'hide' : 'show'
     return (
       <div className="App">
-         <Imageslider />
+      <button onClick={this.handleClick}>{btnText}</button>
+      {this.state.visible ? <Imageslider /> : null}
       </div>
     )
   }
